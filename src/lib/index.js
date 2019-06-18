412976db90bb40271ae485391a4a2e30a7880a0c
askycat <875387716@qq.com>
2019-06-18 16:11:03 +0800

    binding value

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,9 +2,9 @@ export default  {
 	install: function (Vue, options) {
 		Vue.directive("drag", {
 			bind: function (el, binding) {
-				// console.log(binding)
-				el.style.position = "fixed"
-				el.style.zIndex= "999"
+				console.log(binding)
+				el.style.position = binding.value.position||"fixed"
+				el.style.zIndex= binding.value.zIndex||2
 				var isChildDom
 				if (binding.value !== undefined) {
 					isChildDom = true
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-
-
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
